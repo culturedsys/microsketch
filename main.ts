ee9b2a03e4f0402c0adc229fb1ba682e6505649a
Tim Fisken <contact@timfisken.org>
2022-12-18 12:17:40 +0000

    Blink current location

--- main.ts
+++ main.ts
@@ -8,21 +8,25 @@ let x = 0
 x = 2
 y = 2
 basic.forever(function () {
-    led.plot(x, y)
+    led.toggle(x, y)
     basic.pause(500)
     if (input.isGesture(Gesture.TiltLeft)) {
+        led.plot(x, y)
         x += -1
         x = Math.max(x, 0)
     }
     if (input.isGesture(Gesture.TiltRight)) {
+        led.plot(x, y)
         x += 1
         x = Math.min(x, 4)
     }
     if (input.isGesture(Gesture.LogoUp)) {
+        led.plot(x, y)
         y += 1
         y = Math.min(y, 4)
     }
     if (input.isGesture(Gesture.LogoDown)) {
+        led.plot(x, y)
         y += -1
         y = Math.max(y, 0)
     }
