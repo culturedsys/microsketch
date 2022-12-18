8b1bc7aa73f8ffe9af32a4199bc9ec1ff4efe0d9
Tim Fisken <contact@timfisken.org>
2022-12-18 12:37:22 +0000

    Refactor move logic to function

--- main.ts
+++ main.ts
@@ -3,6 +3,16 @@ input.onGesture(Gesture.Shake, function () {
     x = 2
     y = 2
 })
+function move (axis: string, change: number) {
+    led.plot(x, y)
+    if (axis == "x") {
+        x += change
+        x = Math.constrain(x, 0, 4)
+    } else if (axis == "y") {
+        y += change
+        y = Math.constrain(y, 0, 4)
+    }
+}
 let y = 0
 let x = 0
 x = 2
@@ -11,23 +21,15 @@ basic.forever(function () {
     led.toggle(x, y)
     basic.pause(500)
     if (input.isGesture(Gesture.TiltLeft)) {
-        led.plot(x, y)
-        x += -1
-        x = Math.max(x, 0)
+        move("x", -1)
     }
     if (input.isGesture(Gesture.TiltRight)) {
-        led.plot(x, y)
-        x += 1
-        x = Math.min(x, 4)
-    }
-    if (input.isGesture(Gesture.LogoUp)) {
-        led.plot(x, y)
-        y += 1
-        y = Math.min(y, 4)
+        move("x", 1)
     }
     if (input.isGesture(Gesture.LogoDown)) {
-        led.plot(x, y)
-        y += -1
-        y = Math.max(y, 0)
+        move("y", -1)
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        move("y", 1)
     }
 })
